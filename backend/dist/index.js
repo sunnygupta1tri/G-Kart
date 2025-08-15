8104845187122f8a4613bc387140081b0153cdaa
sunnygupta1tri <sunnygupta1tri@gmail.com>
2025-08-15 23:56:26 +0530

    Update 15-8

---
+++ backend/dist/index.js
@@ -0,0 +1,24 @@
+npx ts-node index.ts "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+dotenv_1.default.config();
+const PORT = process.env.PORT || 3000;
+const app = (0, express_1.default)();
+const corsOption = {
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+};
+app.use((0, cors_1.default)(corsOption));
+app.use(express_1.default.json());
+app.use(body_parser_1.default.json());
+app.use((0, cookie_parser_1.default)());
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
